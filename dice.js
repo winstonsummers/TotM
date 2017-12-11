77bc467d48f7016448de04c04593981b001ce222
Winston Summers <winstonsummers@WinstonmatoMacBook-Pro.local>
2017-12-11 11:12:05 -0800

    stub out some html, basic dice rollers in js, some styling

---
+++ dice.js
@@ -0,0 +1,26 @@
+var dice = function(min, max){
+	return Math.floor(Math.random() * (max - min) + min);
+	console.log(dice);
+}
+
+function rollIV() {
+	return dice(1, 4);
+};
+function rollVI() {
+	return dice(1, 6);
+};
+function rollVIII() {
+	return dice(1, 8);
+};
+function rollX() {
+	return dice(1, 10);
+};
+function rollXII() {
+	return dice(1, 12);
+};
+function rollXX() {
+	return dice(1, 20);
+};
+function rollC() {
+	return dice(1, 100);
+};
